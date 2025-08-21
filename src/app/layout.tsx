c1f2d9728fbfaea770285879cdce94514741c983
Alejandro Rivera <arivera.dev@proton.me>
2025-08-21 00:16:47 -0400

    Add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,33 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import localFont from "next/font/local";
+
+export const cervoNeueFont = localFont({
+  src: [
+    {
+      path: "./CervoNeue-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./CervoNeue-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./CervoNeue-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./CervoNeue-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cervoNeueFont.className}>
       <body>
         <Navbar />
         <main>
